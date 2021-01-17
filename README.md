91bc73294fb0c446edf23478fcf73c36388ba968
andreasxp <andres.zhukov@gmail.com>
2021-01-17 21:13:51 +0300

    Add a launch script

--- README.md
+++ README.md
@@ -2,5 +2,17 @@
 It's a hangman console game
 ## Install with pip:
 Console command: `pip install https://github.com/fvbokov/hangman/archive/master.zip`.
-### How to uninstall:
+
+## Launch
+To launch, simply run the hangman module with python. For example, from repository root directory:
+```
+python -m hangman
+```
+Alternatively, hangman can be run with a simple command:
+```
+hangman
+```
+as long as it is installed.
+
+## How to uninstall:
 Console command: `pip uninstall hangman-game`.
--- setup.py
+++ setup.py
@@ -1,12 +1,23 @@
 from setuptools import setup, find_packages
 
 install_requires = []
+extras_require = {
+    "freeze": ["pyinstaller"]
+}
+
+entry_points = {
+    "console_scripts": [
+        "hangman=hangman.__main__:main",
+    ]
+}
 
 setup(
     name="hangman-game",
     version="1.0",
     description="",
     install_requires=install_requires,
+    extras_require=extras_require,
 	package_data={"hangman": ["assets/*.txt"]},
     packages=find_packages(),
+    entry_points=entry_points,
 )
